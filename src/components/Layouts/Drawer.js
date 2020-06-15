66dd584d4b95358a98e1a5897779d83980e49664
100 <bjh9287@naver.com>
2020-06-15 16:19:09 +0900

    testerererer

--- src/components/Layouts/Drawer.js
+++ src/components/Layouts/Drawer.js
@@ -134,7 +134,7 @@ export default withStyles(styles)(
                 <MenuIcon />
               </IconButton>
               <Typography variant="h6" noWrap>
-                Space Reserve System TEST!!! come on!!!
+                Space Reserve System TEST!!! come on!!No! only master
               </Typography>
             </Toolbar>
           </AppBar>
